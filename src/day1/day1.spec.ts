548859bee1bfe7d8d084fdce8b071fa2f060c42b
Lucas David <lucasdavid@hotmail.fr>
2022-12-01 16:16:48 +0100

    setup jest

---
+++ src/day1/day1.spec.ts
@@ -0,0 +1,5 @@
+import day1Temp from ".";
+
+it("Check that jest works", () => {
+  expect(day1Temp(2, 3)).toBe(5);
+});
---
+++ src/day1/index.ts
@@ -0,0 +1,4 @@
+// temporary code just to test the setup of jest
+const day1Temp = (a: number, b: number) => a + b;
+
+export default day1Temp;
